0b14465b5072ce458e16e491c7a4b874886c532a
Dan Murphy <dgemurphy@gmail.com>
2011-04-17 10:44:53 +0100

    after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+D50IMDB::Application.config.secret_token = 'fd2d35d997666661b9b7a2a52ad9853f883776b8319576ad2f991a1d864e2f783e6dde407a826c35c28a0d26c5b77de69b6cebcbd5291040849f6af6836d21cb'
